2b0dccd80290b39b7ecc8f96988974819c2d476c
양성준 <111010564+jakejun98@users.noreply.github.com>
2026-01-03 12:47:13 +0900

    feat: 비밀번호 유효성 검사 추가

--- login.js
+++ login.js
@@ -25,3 +25,36 @@ emailInput.addEventListener('focusout', function() {
     emailErrorMsg.classList.remove('show');   
   }
 });
+
+
+
+const passwordInput = document.getElementById('passwordInput');
+const passwordErrorMsg = document.getElementById('passwordErrorMsg');
+
+passwordInput.addEventListener('focusout', function() {
+  const value = passwordInput.value; // 비밀번호는 공백도 문자로 칠 수 있어서 trim() 안 함
+
+
+  if (value === '') {
+    passwordInput.classList.add('input-error');
+    passwordErrorMsg.textContent = "비밀번호를 입력해주세요.";
+    passwordErrorMsg.classList.add('show');
+  } 
+
+  else if (value.length < 8) {
+    passwordInput.classList.add('input-error');
+    passwordErrorMsg.textContent = "비밀번호를 8자 이상 입력해주세요.";
+    passwordErrorMsg.classList.add('show');
+  } 
+
+  else {
+    passwordInput.classList.remove('input-error');
+    passwordErrorMsg.classList.remove('show');
+  }
+});
+
+
+passwordInput.addEventListener('input', function() {
+  passwordInput.classList.remove('input-error');
+  passwordErrorMsg.classList.remove('show');
+});
