e47a5b6b43f5152c45eaeb5d9eb15198b0a609ad
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-01-21 16:57:55 +0900

    feat: 비밀번호 보기 함수 최적화

--- src/auth.js
+++ src/auth.js
@@ -1,21 +1,22 @@
 const visibilityBtn = document.querySelectorAll('.btn-visibility');
-const passwordInput = document.querySelectorAll(`input[type="password"]`);
-const visibilityImg = document.querySelectorAll('.btn-visibility img');
 
-function toggleVisibility(index) {
-  if (passwordInput[index].type === 'password') {
-    passwordInput[index].type = 'text';
-    visibilityImg[index].src = './images/icon/btn_visibility_on.svg';
-    visibilityImg[index].alt = '비밀번호 숨기기';
-  } else {
-    passwordInput[index].type = 'password';
-    visibilityImg[index].src = './images/icon/btn_visibility_off.svg';
-    visibilityImg[index].alt = '비밀번호 보기';
-  }
-}
-
+// 비밀번호 보기, 숨기기 버튼 조작
 for (let i = 0; i < visibilityBtn.length; i++) {
-  visibilityBtn[i].addEventListener('click', () => {
-    toggleVisibility(i);
+  visibilityBtn[i].addEventListener('click', (event) => {
+    const targetTag = event.target;
+    const inputTag = event.target.parentElement.previousElementSibling;
+
+    if (targetTag.tagName !== 'IMG') return;
+
+    // 버튼 클릭시 이미지, input type 변경
+    if (inputTag.type != 'text') {
+      targetTag.src = './images/icon/btn_visibility_on.svg';
+      targetTag.alt = '비밀번호 숨기기';
+      inputTag.type = 'text';
+    } else {
+      targetTag.src = './images/icon/btn_visibility_off.svg';
+      targetTag.alt = '비밀번호 보기';
+      inputTag.type = 'password';
+    }
   });
 }
