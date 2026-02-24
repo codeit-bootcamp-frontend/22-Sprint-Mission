46a3688f97b796a6bb13fbdc45f6ad1ee1a48f7a
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-02-24 12:42:17 +0900

    feat: 피드백 반영

--- src/auth.js
+++ src/auth.js
@@ -1,22 +1,22 @@
-const visibilityBtn = document.querySelectorAll('.btn-visibility');
+const visibilityBtn = document.querySelectorAll(".btn-visibility");
 
 // 비밀번호 보기, 숨기기 버튼 조작
-for (let i = 0; i < visibilityBtn.length; i++) {
-  visibilityBtn[i].addEventListener('click', (event) => {
+visibilityBtn.forEach((ele) => {
+  ele.addEventListener("click", (event) => {
     const targetTag = event.target;
     const inputTag = event.target.parentElement.previousElementSibling;
 
-    if (targetTag.tagName !== 'IMG') return;
+    if (targetTag.tagName !== "IMG") return;
 
     // 버튼 클릭시 이미지, input type 변경
-    if (inputTag.type != 'text') {
-      targetTag.src = './images/icon/btn_visibility_on.svg';
-      targetTag.alt = '비밀번호 숨기기';
-      inputTag.type = 'text';
+    if (inputTag.type != "text") {
+      targetTag.src = "./images/icon/btn_visibility_on.svg";
+      targetTag.alt = "비밀번호 숨기기";
+      inputTag.type = "text";
     } else {
-      targetTag.src = './images/icon/btn_visibility_off.svg';
-      targetTag.alt = '비밀번호 보기';
-      inputTag.type = 'password';
+      targetTag.src = "./images/icon/btn_visibility_off.svg";
+      targetTag.alt = "비밀번호 보기";
+      inputTag.type = "password";
     }
   });
-}
+});
