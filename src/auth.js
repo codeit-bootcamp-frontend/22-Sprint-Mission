17a473ef59956f133289b3f82df56ff3c23b6dd9
YEO Sugyeong <125259910+luneyeo@users.noreply.github.com>
2026-02-24 12:48:39 +0900

    feat: 비밀번호 함수 최적화

--- src/auth.js
+++ src/auth.js
@@ -1,22 +1,15 @@
 const visibilityBtn = document.querySelectorAll(".btn-visibility");
 
 // 비밀번호 보기, 숨기기 버튼 조작
-visibilityBtn.forEach((ele) => {
-  ele.addEventListener("click", (event) => {
-    const targetTag = event.target;
-    const inputTag = event.target.parentElement.previousElementSibling;
+visibilityBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const input = btn.previousElementSibling;
+    const img = btn.querySelector("img");
 
-    if (targetTag.tagName !== "IMG") return;
+    const isPassword = input.type === "password";
 
-    // 버튼 클릭시 이미지, input type 변경
-    if (inputTag.type != "text") {
-      targetTag.src = "./images/icon/btn_visibility_on.svg";
-      targetTag.alt = "비밀번호 숨기기";
-      inputTag.type = "text";
-    } else {
-      targetTag.src = "./images/icon/btn_visibility_off.svg";
-      targetTag.alt = "비밀번호 보기";
-      inputTag.type = "password";
-    }
+    input.type = isPassword ? "text" : "password";
+    img.src = `./images/icon/btn_visibility_${isPassword ? "on" : "off"}.svg`;
+    img.alt = isPassword ? "비밀번호 숨기기" : "비밀번호 보기";
   });
 });
